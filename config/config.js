076931ff0d8ba9427ebee7f4e40d59e5f245e289
jcr <jcrascon+github@gmail.com>
2021-01-13 21:38:32 -0800

    add environment variables

---
+++ config/config.js
@@ -0,0 +1,24 @@
+export const dbConfig = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
+  },
+  production: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.PORT,
+    host: process.env.HOST,
+    dialect: process.env.DB_DIALECT
+  }
+}
