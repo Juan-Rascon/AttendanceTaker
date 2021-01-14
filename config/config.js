40852a685970f7640725bd04f660c5dec00bd69b
jcr <jcrascon+github@gmail.com>
2021-01-13 22:00:46 -0800

    updated database config

--- config/config.js
+++ config/config.js
@@ -1,17 +1,23 @@
-export const dbConfig = {
+
+if (process.env.NODE_ENV !== 'production'){
+  require('dotenv').config();
+}
+
+
+module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: 'mysql'
   },
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: 'mysql'
   },
   production: {
     username: process.env.USERNAME,
@@ -19,6 +25,6 @@ export const dbConfig = {
     database: process.env.DATABASE,
     port: process.env.PORT,
     host: process.env.HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: 'mysql'
   }
 }
